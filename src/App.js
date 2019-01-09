d903d0c5c539c07fa0dee4b1a2a953509718dd1a
Nguyen Quang An <ngquangan@gmail.com>
2019-01-09 07:52:48 +0700

    Initial project

--- src/App.js
+++ src/App.js
@@ -3,9 +3,12 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+
+  
   render() {
     return (
-      <div className="App">
+      <div className="App" ref = {}>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
